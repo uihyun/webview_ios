e15c685e3bae225b1d18f5905242b1bc9e9919fc
uihyun <uihyunkei@gmail.com>
2020-10-29 01:47:33 +0900

    add the latest changes calling the status bar

--- WebViewer/AppDelegate.swift
+++ WebViewer/AppDelegate.swift
@@ -17,14 +17,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        if let statusbar = UIApplication.shared.value(forKey: "statusBar") as? UIView {
-            let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
-            let blurView = UIVisualEffectView(effect: blurEffect)
-            blurView.frame = statusbar.bounds
-            blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            statusbar.addSubview(blurView)
-            statusbar.sendSubviewToBack(blurView)
-        }
+        var statusBarUIView: UIView? {
+            if #available(iOS 13.0, *) {
+                let tag = 3848245
+                let keyWindow = UIApplication.shared.connectedScenes
+                    .map({$0 as? UIWindowScene})
+                    .compactMap({$0})
+                    .first?.windows.first
+
+                if let statusBar = keyWindow?.viewWithTag(tag) {
+                    return statusBar
+                } else {
+                    let height = keyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
+                    let statusBarView = UIView(frame: height)
+                    statusBarView.tag = tag
+                    statusBarView.layer.zPosition = 999999
+
+                    keyWindow?.addSubview(statusBarView)
+                    return statusBarView
+                }
+            } else {
+                if responds(to: Selector(("statusBar"))) {
+                    return value(forKey: "statusBar") as? UIView
+                }
+            }
+            return nil
+          }
+        
+        // old one
+//        if let statusbar = UIApplication.shared.value(forKey: "statusBar") as? UIView {
+//            let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+//            let blurView = UIVisualEffectView(effect: blurEffect)
+//            blurView.frame = statusbar.bounds
+//            blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//            statusbar.addSubview(blurView)
+//            statusbar.sendSubviewToBack(blurView)
+//        }
         
         return true
     }
